01fc4db7654899939965cb43139221a6a37fb859
ToddSandberg <sandbergs.todd@gmail.com>
2023-09-16 13:14:24 -0700

    Allow typescript and add tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Tabs from './common/Tabs';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Tabs/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
