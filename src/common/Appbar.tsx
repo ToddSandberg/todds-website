d004b8ae6cf878ef59967605ee3ad132bb51d07c
ToddSandberg <sandbergs.todd@gmail.com>
2023-09-20 13:16:08 -0700

    Add some CSS for tabs

---
+++ src/common/Appbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Appbar() {
+    return <div
+        style={{
+            width: '100%',
+            height: '5vh'
+        }}
+    />;
+}
--- src/common/Tabs.tsx
+++ src/common/Tabs.tsx
@@ -1,17 +1,24 @@
 import { Tab } from "@headlessui/react";
 import React from 'react'
+import Home from "../home/Home";
+import Appbar from "./Appbar";
 
 export default function Tabs() {
     return <Tab.Group>
-    <Tab.List>
-      <Tab>Tab 1</Tab>
-      <Tab>Tab 2</Tab>
-      <Tab>Tab 3</Tab>
-    </Tab.List>
-    <Tab.Panels>
-      <Tab.Panel>Content 1</Tab.Panel>
-      <Tab.Panel>Content 2</Tab.Panel>
-      <Tab.Panel>Content 3</Tab.Panel>
-    </Tab.Panels>
-  </Tab.Group>;
+        <Tab.List>
+          <div className="Appbar">
+            <Tab>Home</Tab>
+            <Tab>About Me</Tab>
+            <Tab>Personal Projects</Tab>
+          </div>
+        </Tab.List>
+        <Tab.Panels>
+          <Appbar/>
+          <div className="App-content">
+            <Tab.Panel><Home/></Tab.Panel>
+            <Tab.Panel>Content 2</Tab.Panel>
+            <Tab.Panel>Content 3</Tab.Panel>
+          </div>
+        </Tab.Panels>
+      </Tab.Group>;
 }
