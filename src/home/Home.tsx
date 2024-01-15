26f3273ec3dc2f829be29467ecf6260530d47d25
ToddSandberg <sandbergs.todd@gmail.com>
2024-01-15 15:20:28 -0800

    Adds main body for main page

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -13,8 +13,19 @@ export default function Home({backgroundColor, currentPercent}: Props) {
     return <div style={{width: '100%'}}>
         <h3 style={{fontFamily: 'bit'}}>Welcome to my website.</h3>
         <SunAndMoon currentPercent={currentPercent}/>
-        <div style={{height: 1200}}>
-            test
+        <div style={{alignContent: 'center', justifyContent: 'center', display: 'flex'}}>
+            <div style={{zIndex: '2', color:'#282c34', backgroundColor: 'white', width: 500}}>
+                <p style={{padding: 20}}>I'm passionate about developing...</p>
+                <div style={{ paddingBottom: 600 }}>
+                    Video Games
+                </div>
+                <div style={{ paddingBottom: 600 }}>
+                    Video Games
+                </div>
+                <div style={{ paddingBottom: 600 }}>
+                    Industry Grade Software
+                </div>
+            </div>
         </div>
         <div style={{position: 'sticky', bottom: '0', overflow: 'clip', zIndex: '1'}}>
             <Hills fill={backgroundColor} className="Hills" style={{zIndex: '1', position: 'sticky', width: '100%', bottom: '0'}}/>
