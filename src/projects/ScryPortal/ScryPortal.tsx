64e851352f96809e62ae5cbc4bed1f8297290832
ToddSandberg <sandbergs.todd@gmail.com>
2024-02-02 10:55:09 -0800

    Add link for scryportal

--- src/projects/ScryPortal/ScryPortal.tsx
+++ src/projects/ScryPortal/ScryPortal.tsx
@@ -20,6 +20,7 @@ export default function ScryPortal() {
                         I've always had a passion for optimizing productivity and removing the time it takes to plan various aspects of life.
                         This react application is intended to ease that burden by tracking your goals and automatically scheduling your day.
                     </p>
+                    <a href="https://www.scryportal.com/">Sign up and use</a>
                 </div>
             </Tabs>
         </header>
