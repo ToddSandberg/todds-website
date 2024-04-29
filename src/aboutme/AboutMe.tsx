5647921fd032f4f0a383bf7bc970bd348ba9c7b3
ToddSandberg <sandbergs.todd@gmail.com>
2024-04-29 10:15:37 -0700

    Add a little typing effect

--- src/aboutme/AboutMe.tsx
+++ src/aboutme/AboutMe.tsx
@@ -24,6 +24,7 @@ export default function AboutMe() {
                             {'I\'m a full stack developer who likes to make cool things.' +
                             'Over my career I helped build various software products at Amazon, including web apps for delivery stations, back end workflows for enabling both sub same day and global package delivery, and Alexa presence detection technology.' + 
                             'I\'ve also worked on various passion projects, which I invite you to explore the <b><u>Projects</u></b> tab.'}
+                            <span className='typewriter-caret'></span>
                         </p>
                     </>
                     :<div>
@@ -32,6 +33,7 @@ export default function AboutMe() {
                             {'I\'m a full stack developer who likes to make cool things.'+
                             'Over my career I helped build various software products at Amazon, including web apps for delivery stations, back end workflows for enabling both sub same day and global package delivery, and Alexa presence detection technology.'+
                             'I\'ve also worked on various passion projects, which I invite you to explore the '}<b><u>Projects</u></b>{' tab.'}
+                            <span className='typewriter-caret'></span>
                         </p>
                     </div>}
                 <p style={{fontSize: isSmallDevice() ? '14px' : '20px'}}>
@@ -46,6 +48,7 @@ export default function AboutMe() {
                     </ul>
                     {'I\'ve also used AWS cloud services extensively to create enterprise workflows, API microservices, and host web applications.' +
                     'My primary back end languages I use are Java, C#, and Typescript.'}
+                    <span className='typewriter-caret'></span>
                 </p>
             </Tabs>
         </header>
