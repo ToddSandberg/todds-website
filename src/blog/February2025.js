bc62fd521344595151b6ea47cf3678b2b18a923d
ToddSandberg <sandbergs.todd@gmail.com>
2025-03-22 10:59:52 -0700

    feb 2025 + dependency upgrades

---
+++ src/blog/February2025.js
@@ -0,0 +1,61 @@
+export const february2025 = `# February 2025
+
+Not too many updates this month been busy with life.
+
+### Scryportal
+Scryportal is a task keeping and scheduling website I've been continuously developing: https://www.scryportal.com/
+- More typescript conversions and more tests
+- Moving common stuff to different libraries
+- Working on creating workout component
+- Starting to work on a study component
+- Starting to work on a finance component
+
+### AI Server
+Creating a dedicated server for saving context for different profiles for AI
+- Working on allowing playing sounds on triggers
+- Working on lots of integrations
+- Integrated with chatgpt, perplexity, grok
+- Make sure audio does not overlap if multiple requests
+- Image recognition add yes message/no message to image recognition
+- Added a sound when start listening
+- Working on allowing AI agents to talk to each other
+
+### Chatbot
+Creating an ai chatbot mostly for experimentation with more front end techonology.
+- Change conversations to be list on the side instead of dropdown
+- Show red dot on conversations/characters if unread
+
+### AI Profile Creator
+Creating a ui for the AI Server to create profiles/characters for ai to separate out different context/functions to specific conversations that need them
+- Improved mobile ui
+- Make character select scrollable
+- Working on allowing creating quests
+- Fix not being able to change profile description
+
+### Pin board app
+Creating a simple webb app to act like a digital pin board.
+- Added ability to delete items
+- Added ability to resize items
+
+### AI Desktop Pal
+Creating a game where you can have a custom dekstop pal ala clippy who comments on your usage. Planning to release soon.
+- Added physics on dragging
+- Added ability to highlight through chrome extension and generate tts for highlight (also allow explain/summarize)
+- On drag of character set image to dragging image
+- Truncate displaying text if greater than 200 characters
+
+### AI Social Media
+Working on creating a social media that pulls in stuff you are interested in and generate a social media feed using AI.
+- Added ability to generate posts
+- Added db to store posts
+
+### Steamchievos
+Small php app created to provide suggestions on what steam achievements to go for next.
+- Fix ignore game on game list for gog games
+- On load save somewhere the last found state somewhere so it can be fetched quickly
+- Working on allowing adding other types than steam to playlist
+
+### DND AI Chatbot
+Created a UI for playing dnd using AI: https://github.com/ToddSandberg/DND-AI
+- Fix Prototype-polluting assignment vulnerability
+`;
