20d100cbb7a8680b6b841b59ef7e9f9aad773cea
ToddSandberg <sandbergs.todd@gmail.com>
2025-08-31 08:39:01 -0700

    July 2025 blog

---
+++ src/blog/July2025.js
@@ -0,0 +1,22 @@
+export const june2025 = `# July 2025
+
+### Scryportal
+Scryportal is a task keeping and scheduling website I've been continuously developing: https://www.scryportal.com/
+- Workout: Highlight best pace and best distance for runs
+- CRM: Working on adding a relationship manager page
+- Meal Diary: Added some ui animations
+- Working on adding a new task view that will be a web like structure for breaking down tasks
+- More Tests
+
+### AI Profile Creator
+Creating a ui for the AI Server to create profiles/characters for ai to separate out different context/functions to specific conversations that need them
+- Working on allowing configuring quests to complete
+- Working on allowing a fake inventory for AI's
+- Add toggle to hide profile in other applications
+
+### Inventory UI
+Creating a website to keep track of all of the things you own and run analytics on it
+- Working on suggesting tags with AI
+- Working on some more specific attributes for books
+
+`;
